045da793a64a90b22ad5b1944345952eac63fb45
zhongxiaodong <zxdzyadmin@163.com>
2018-03-02 00:43:38 +0800

    1、新增加密crypto-js库；2、新增linq-es2015库

--- src/components/FindVC.js
+++ src/components/FindVC.js
@@ -2,10 +2,11 @@
  * @Author: zhongxd 
  * @Date: 2018-02-01 15:37:54 
  * @Last Modified by: zhongxd
- * @Last Modified time: 2018-02-01 15:38:26
+ * @Last Modified time: 2018-03-01 23:52:51
  */
 
 import React, { Component } from 'react';
+import CryptoJS from 'crypto-js';
 import {
     AppRegistry,
     StyleSheet,
@@ -19,6 +20,16 @@ export default class FindVC extends Component {
         headerTitle: '发现',
         tabBarLabel: '发现',
         tabBarIcon: <View style={{ height: 30, width: 30, backgroundColor: 'red' }}></View>
+    };
+    componentDidMount() {
+        // Encrypt
+        var ciphertext = CryptoJS.AES.encrypt('my message', 'secret key 123');
+        console.log(ciphertext);
+        // Decrypt
+        var bytes = CryptoJS.AES.decrypt(ciphertext.toString(), 'secret key 123');
+        var plaintext = bytes.toString(CryptoJS.enc.Utf8);
+        alert("解密后：" + plaintext);
+        console.log(plaintext);
     };
     render() {
         const { navigate } = this.props.navigation;
--- src/components/NewsVC.js
+++ src/components/NewsVC.js
@@ -1,11 +1,12 @@
 /*
  * @Author: zhongxd 
  * @Date: 2018-02-01 15:39:11 
- * @Last Modified by:   zhongxd 
- * @Last Modified time: 2018-02-01 15:39:11 
+ * @Last Modified by: zhongxd
+ * @Last Modified time: 2018-03-02 00:29:09
  */
 
 import React, { Component } from 'react';
+import {asEnumerable, Range} from "linq-es2015";
 import {
     AppRegistry,
     StyleSheet,
@@ -13,12 +14,19 @@ import {
     View,
     TouchableOpacity
 } from 'react-native';
+
 export default class NewsVC extends Component {
 
     static navigationOptions = {
         headerTitle: '消息',
         tabBarLabel: '消息',
         tabBarIcon: <View style={{ height: 30, width: 30, backgroundColor: 'red' }}></View>
+    };
+    componentDidMount(){
+        var count =  asEnumerable( [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] )
+                       .Where(a => a % 2 == 1)
+                       .Count();
+        alert(count);
     };
     render() {
         const { navigate } = this.props.navigation;
