268572358252039ad7d5750c8900f7a5f3a1b5bf
zhongxiaodong <zxdzyadmin@yonyou.com>
2018-03-24 16:36:53 +0800

    增加下拉框组件

--- src/components/AddsVC.js
+++ src/components/AddsVC.js
@@ -25,28 +25,36 @@ import {
     Platform,
     KeyboardAvoidingView
 } from 'react-native';
+import ModalDropdown from 'react-native-modal-dropdown';
 import EditView from './EditView';
 import LoginButton from './LoginButton';
 import RealmDB from './RealmDB';
 import CryptoJS from 'crypto-js';
 import Utils from './Utils';
 
+
 let homeImages = require('../images/icon_contacts_line_light.png');
+
 
 
 export default class AddsVC extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            account_name:'',//应用名称、网站名称
-            login_name_one:'',//第一登录名
-            login_name_two:'',//第二登录名
-            login_pwd:''//登录密码
+            account_name: '',//应用名称、网站名称
+            login_name_one: '',//第一登录名
+            login_name_two: '',//第二登录名
+            login_pwd: ''//登录密码
         }
-        
+
     };
-    
-    saveData = () =>{
+
+    componentDidMount() {
+       
+    };
+
+   
+    saveData = () => {
         console.log(this.state.account_name);
         console.log(this.state.login_name_one);
         console.log(this.state.login_name_two);
@@ -54,18 +62,18 @@ export default class AddsVC extends React.Component {
         // Encrypt
         let encryptLoginPwd = CryptoJS.AES.encrypt(this.state.login_pwd, 'secret key zhongxd');
         let login_pwd = encryptLoginPwd.toString();
-        RealmDB.write(()=>{
+        RealmDB.write(() => {
             RealmDB.create(
                 'AccountInfo',
                 {
-                    id:Utils.UUID(),
-                    account_name:this.state.account_name,
-                    login_name_one:this.state.login_name_one,
-                    login_name_two:this.state.login_name_two,
-                    login_pwd:login_pwd
+                    id: Utils.UUID(),
+                    account_name: this.state.account_name,
+                    login_name_one: this.state.login_name_one,
+                    login_name_two: this.state.login_name_two,
+                    login_pwd: login_pwd
                 }
             );
-            
+
         })
     };
     render() {
@@ -74,11 +82,11 @@ export default class AddsVC extends React.Component {
                 <ScrollView>
                     <View style={LoginStyles.loginview}>
                         <View style={{
-                            flexDirection: 'row', height: 80, marginTop: 1,
+                            flexDirection: 'row', height: 40, marginTop: 1,
                             justifyContent: 'center',
                             alignItems: 'flex-start',
                         }}>
-                            <Image source={homeImages} />
+                            <Image source={homeImages} style={{ width: 40, height: 40 }} />
                         </View>
                         <View style={{ marginTop: 5 }}>
                             {/*  <EditView name='输入用户名/注册手机号' onChangeText={(text) => {
@@ -87,40 +95,45 @@ export default class AddsVC extends React.Component {
                                 <EditView name='输入密码' onChangeText={(text) => {
                                     this.password = text;
                                 }} /> */}
-                                <TextInput placeholder='输入应用名称/网站名称' 
-                                    style={LoginStyles.TextInput} 
-                                    onChangeText={(account_name) => this.setState({account_name})}  
-                                    value={this.state.account_name} >
-                                </TextInput>
-                                <TextInput placeholder='输入第一登录账号' 
-                                    style={LoginStyles.TextInput}
-                                    onChangeText={(login_name_one) => this.setState({login_name_one})}  
-                                    value={this.state.login_name_one}>
-                                </TextInput>
-                                <TextInput placeholder='输入第二登录账号' 
-                                    style={LoginStyles.TextInput}
-                                    onChangeText={(login_name_two) => this.setState({login_name_two})}  
-                                    value={this.state.login_name_two}>
-                                </TextInput>
-                                <TextInput placeholder='输入密码' 
-                                    style={LoginStyles.TextInput} 
-                                    secureTextEntry={true}
-                                    onChangeText={(login_pwd) => this.setState({login_pwd})}  
-                                    value={this.state.login_pwd}>
-                                </TextInput>
-                                {/*  <LoginButton name='登录' onPressCallback={this.onPressCallback} /> */}
-                                <TouchableOpacity  style={LoginStyles.loginTextView} 
-                                    onPress={this.saveData}>
-                                    <Text style={LoginStyles.loginText} >
-                                        保存  
+                            <ModalDropdown options={['option 1', 'option 2']}
+                                    style={{width:200,border:1,borderColor:'#000000',backgroundColor:'#EE4000'}}
+                                    dropdownStyle={{border:1,borderColor:'#000000',height:200}}
+                                 />
+
+                            <TextInput placeholder='输入应用名称/网站名称'
+                                style={LoginStyles.TextInput}
+                                onChangeText={(account_name) => this.setState({ account_name })}
+                                value={this.state.account_name} >
+                            </TextInput>
+                            <TextInput placeholder='输入第一登录账号'
+                                style={LoginStyles.TextInput}
+                                onChangeText={(login_name_one) => this.setState({ login_name_one })}
+                                value={this.state.login_name_one}>
+                            </TextInput>
+                            <TextInput placeholder='输入第二登录账号'
+                                style={LoginStyles.TextInput}
+                                onChangeText={(login_name_two) => this.setState({ login_name_two })}
+                                value={this.state.login_name_two}>
+                            </TextInput>
+                            <TextInput placeholder='输入密码'
+                                style={LoginStyles.TextInput}
+                                secureTextEntry={true}
+                                onChangeText={(login_pwd) => this.setState({ login_pwd })}
+                                value={this.state.login_pwd}>
+                            </TextInput>
+                            {/*  <LoginButton name='登录' onPressCallback={this.onPressCallback} /> */}
+                            <TouchableOpacity style={LoginStyles.loginTextView}
+                                onPress={this.saveData}>
+                                <Text style={LoginStyles.loginText} >
+                                    保存
                                     </Text>
-                                </TouchableOpacity>
-                            <Text style={{ color: "#4A90E2",  marginTop: 10 }} >提示：数据只保存到手机本地，不会访问网络</Text>
+                            </TouchableOpacity>
+                            <Text style={{ color: "#4A90E2", marginTop: 10 }} >提示：数据只保存到手机本地，不会访问网络</Text>
                         </View>
                     </View>
                 </ScrollView>
             </KeyboardAvoidingView>
-            
+
         )
     }
 }
@@ -150,16 +163,16 @@ const LoginStyles = StyleSheet.create({
     },
     loginText: {
         color: '#ffffff',
-         fontWeight: 'bold',
-         width:30,
-      },
-      loginTextView: {
+        fontWeight: 'bold',
+        width: 30,
+    },
+    loginTextView: {
         marginTop: 10,
-        height:50,
+        height: 50,
         backgroundColor: '#3281DD',
-        borderRadius:5,
+        borderRadius: 5,
         flexDirection: 'row',
         justifyContent: 'center',
-        alignItems:'center',
-      },
+        alignItems: 'center',
+    },
 });
